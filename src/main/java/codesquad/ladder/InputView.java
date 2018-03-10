7b571836d82ff2259e7a11c1e0ec7586e41a1a39
larryjung <corbswjd@gmail.com>
2018-03-10 17:33:59 +0900

    step2 commit

--- src/main/java/codesquad/ladder/InputView.java
+++ src/main/java/codesquad/ladder/InputView.java
@@ -1,42 +1,45 @@
 package codesquad.ladder;
 
-import java.util.InputMismatchException;
+import codesquad.ladder.exceptions.InvalidNumPeopleException;
+import codesquad.ladder.exceptions.InvalidSizeLadderException;
 import java.util.Scanner;
 
 public class InputView {
 
-    private static boolean isInvalidInputNum(int num) {
-        return num < 2;
-    }
+    private static int NUM_PEOPLE;
+
+    private static int SIZE_LADDER;
+
+    private static final int MIN_PEOPLE_NUMBER = 2;
+
+    private static final int MIN_LADDER_SIZE = 2;
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public static int getNumPeople() {
-        int numPeople = 1;
-        System.out.println("시작 시 엔터를 눌러주세요.");
-        while (isInvalidInputNum(numPeople)) {
-            try {
-                scanner.nextLine(); // this is about \n scanner error
-                System.out.println("사람은 몇 명 인가요? 2이상의 값을 입력해 주세요.\n>>");
-                numPeople = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("숫자 값을 넣어주세요.");
+    public static int getNumPeople(){
+        try {
+            System.out.println("사람은 몇 명 인가요? 2이상의 값을 입력해 주세요.\n>>");
+            NUM_PEOPLE = Integer.parseInt(scanner.nextLine());
+            if (NUM_PEOPLE < MIN_PEOPLE_NUMBER) {
+                throw new InvalidNumPeopleException();
             }
+        } catch (Exception e) {
+            getNumPeople();
         }
-        return numPeople;
+        return NUM_PEOPLE;
     }
 
-    public static int getNumLadder() {
-        int numLadder = 1;
-        while (isInvalidInputNum(numLadder)) {
-            try {
-                scanner.nextLine(); // this is about \n scanner error
-                System.out.println("사다리는 몇 개 인가요? 2이상의 값을 입력해 주세요.\n>>");
-                numLadder = scanner.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("숫자 값을 넣어주세요.");
+    public static int getSizeLadder(){
+        try {
+            System.out.println("사다리는 몇 개 인가요? 2이상의 값을 입력해 주세요.\n>>");
+            SIZE_LADDER = Integer.parseInt(scanner.nextLine());
+            if (SIZE_LADDER < MIN_LADDER_SIZE) {
+                throw new InvalidSizeLadderException();
             }
+        } catch (Exception e) {
+            getSizeLadder();
         }
-        return numLadder;
+        return SIZE_LADDER;
     }
 }
+
--- src/main/java/codesquad/ladder/LadderMain.java
+++ src/main/java/codesquad/ladder/LadderMain.java
@@ -3,9 +3,9 @@ package codesquad.ladder;
 public class LadderMain {
     public static void main(String[] args) {
         int numPeople = InputView.getNumPeople();
-        int numLadder = InputView.getNumLadder();
+        int sizeLadder = InputView.getSizeLadder();
 
-        Ladder ladder = new Ladder(numPeople, numLadder);
+        Ladder ladder = new Ladder(numPeople, sizeLadder);
         ResultView.printLadder(ladder);
     }
 }
